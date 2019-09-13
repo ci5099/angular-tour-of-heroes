cc0f0c86dee1017f2a81ba43f528cbbab2ae9e89
Christian Iosif <ciosif@lombardinternational.com>
2019-09-13 13:17:08 -0400

    Angular Fundamentals Tours of Heroes App HTTP Section -Added search box -Holds information using in-app-storage as a simulated database -Send and get objects using HTTP

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {Hero} from '../hero';
 import {ActivatedRoute} from '@angular/router';
 import {Location} from '@angular/common';
 
+import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
 
 @Component({
@@ -30,7 +30,12 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
-  goBack() {
+  goBack(): void {
     this.location.back();
+  }
+
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
   }
 }
